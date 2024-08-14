798c6170f234cade5c08c0f337acd44dcede7d3c
Florian Hass <f91hama@gmail.com>
2024-08-14 16:21:20 +0000

    Added post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-fgit24-djangoblog-an35gf0rjl3.ws.codeinstitute-ide.net', '.herokuapp.com']
 
