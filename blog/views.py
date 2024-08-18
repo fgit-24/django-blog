90347fcf98a502050999b70a8abf64db8a950177
Florian Hass <f91hama@gmail.com>
2024-08-18 15:08:28 +0000

    Add post function without admin

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,10 @@ from .forms import CommentForm
 
 # Create your views here.
 
-
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
-
 
 def post_detail(request, slug):
     """
@@ -25,13 +23,26 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-def post_detail(request, slug):
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
-    comment_form = CommentForm()
+    
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+            
+            # Redirect or add a success message here if needed
+    else:
+        comment_form = CommentForm()
 
     return render(
         request,
@@ -41,5 +52,5 @@ def post_detail(request, slug):
             "comments": comments,
             "comment_count": comment_count,
             "comment_form": comment_form,
-        },
+        }
     )
